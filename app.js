8f6a99c35207dbf0896368296df0c0bdb2fbff43
sergio fabian fierro gonzalez <sfierrogonzalez03@gmail.com>
2025-08-27 11:13:11 -0500

    crear-codigo js

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto 
+//y añadirlo a la lista de amigos creada anteriormente.
+
+let amigos = []
+
+function agregarAmigo(Elemento, texto) {
+    let nombre = String(document.getElementById('amigo').value);
+    
+    console.log(nombre);
+
+    if (nombre == ''){
+
+alert("Por favor, inserte un nombre.")
+        return;
+}
+function mostrarLista() {
+    let lista = document.querySelector("# listaAmigos");
+    lista.innerHTML = "";
+
+
+}
+    amigos.push(nombre);
+    
+    console.log(amigos);
+
+    
+
+
+
+ 
+ 
+ };
+    
+    
+
+
